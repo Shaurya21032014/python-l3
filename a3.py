ff41c1320709b044a67b934714b39a30ad71a61e
Shaurya21032014 <haritika.arora@gmail.com>
2025-11-08 15:41:42 +0530

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,8 @@
+number = int(input("enter your number"))
+if(number > 50):
+    if(number%2==0):
+      print("the number is even ")
+    else:
+       print("the number is odd")
+else:
+   print("sorry we cont check even odd cause the number u have entered , it is less thsn 50")
